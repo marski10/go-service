df54249795ad7c3f88498350b723c07857619e23
marksi10 <marquinhus.bill@hotmail.com>
2024-05-25 13:20:35 -0300

    go-innittial

---
+++ src/consumer.go
@@ -0,0 +1,22 @@
+package main 
+import {"fmt"
+		 "net/http"
+		"os"
+		"io"
+		"log"
+		}
+
+func healthCheck(){
+	
+	
+}
+
+
+
+func main() {
+
+	fmt.Println("Hello, World!")
+
+	healthCheck()
+
+}
